9729dafbd564db41694aa61921e5895f812c1363
manith003 <manithkumar8190@gmail.com>
2025-05-12 21:59:34 +0530

    some changes made is updated

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
 import { gsap } from "https://cdn.skypack.dev/gsap";
 import { CustomEase } from "https://cdn.skypack.dev/gsap/CustomEase";
 
+
 gsap.registerPlugin(CustomEase);
 CustomEase.create("hop", "0.9, 0,0.1,1");
 
-function loading(){
-document.addEventListener("DOMContentLoaded", () => {
+
+function loading() {
+  document.addEventListener("DOMContentLoaded", () => {
     const tl = gsap.timeline({
-        delay: 0.3,
-        defaults: {
-            ease: "hop"
-        },
+      delay: 0.3,
+      defaults: {
+        ease: "hop",
+      },
     });
 
     const counts = document.querySelectorAll(".count");
-    counts.forEach((count, index)=>{
-        const digits = count.querySelectorAll(".digit h1");
+    counts.forEach((count, index) => {
+      const digits = count.querySelectorAll(".digit h1");
 
-        tl.to(digits,{
-            y:"0%",
+      tl.to(
+        digits,
+        {
+          y: "0%",
+          duration: 1,
+          stagger: 0.075,
+        },
+        index * 1
+      );
+
+      if (index < counts.length) {
+        tl.to(
+          digits,
+          {
+            y: "-100%",
             duration: 1,
-            stagger: 0.075
-        },index*1);
-
-        if(index<counts.length){
-            tl.to(digits,{
-                y:"-100%",
-                duration: 1,
-                stagger: 0.075
-            },index*1+1);
-        }
-    })
-
-    tl.to(".spinner",{
-        opacity: 0,
-        duration: 0.5,
+            stagger: 0.075,
+          },
+          index * 1 + 1
+        );
+      }
     });
-    tl.to(".word h1",{
+
+    tl.to(".spinner", {
+      opacity: 0,
+      duration: 0.5,
+    });
+    tl.to(
+      ".word h1",
+      {
         y: "0%",
         duration: 1,
-    },"<");
+      },
+      "<"
+    );
 
     // tl.to(".divider",{
     //     scaleY: "100%",
@@ -47,44 +61,60 @@ document.addEventListener("DOMContentLoaded", () => {
     //     onComplete: () => gsap.to(".divider", {opacity: 0, duration: 0.4, delay: 0.3}),
     // });
 
-    tl.to("#word-1 h1",{
-        y: "120%",
-        duration: 0.9,
-        delay: 0.3
+    tl.to("#word-1 h1", {
+      y: "120%",
+      duration: 0.9,
+      delay: 0.3,
     });
 
-    tl.to("#word-2 h1",{
+    tl.to(
+      "#word-2 h1",
+      {
         y: "-120%",
         duration: 0.9,
-    },"<");
+      },
+      "<"
+    );
 
-    tl.to(".block",{
-        clipPath: "polygon(0 0, 100% 0, 100% 0%, 0 0%)",
-        duration: 1,
-        stagger: 0.1,
-        delay: 0.75,
-        onStart: () => gsap.to(".hero-img", {scale: 1, duration: 2, ease: "hop"})
-    })
+    tl.to(".block", {
+      clipPath: "polygon(0 0, 100% 0, 100% 0%, 0 0%)",
+      duration: 1,
+      stagger: 0.1,
+      delay: 0.75,
+      onStart: () =>
+        gsap.to(".hero-img", { scale: 1, duration: 2, ease: "hop" }),
+    });
 
-    tl.to([".nav",".line h1",".line p"],{
-        y:"0%",
+    tl.to(
+      [".nav", ".line h1", ".line p"],
+      {
+        y: "0%",
         duration: 1.5,
         stagger: 0.2,
-    },"<");
+      },
+      "<"
+    );
 
-    tl.to([".cta", ".cta-icon"],{
+    tl.to(
+      [".cta", ".cta-icon"],
+      {
         scale: 1,
-        duration:1.5,
+        duration: 1.5,
         stagger: 0.75,
-        delay: 0.75
-    },"<");
+        delay: 0.75,
+      },
+      "<"
+    );
 
-    tl.to(".cta-label p",{
-        y:"0%",
+    tl.to(
+      ".cta-label p",
+      {
+        y: "0%",
         duration: 1.5,
         delay: 0.5,
-    },"<");
-});
+      },
+      "<"
+    );
+  });
 }
 loading();
-
