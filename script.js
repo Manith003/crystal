1b954a419e15c9083b1bc11344276e4adae974cc
manith003 <manithkumar8190@gmail.com>
2025-05-26 19:21:25 +0530

    swiper js added

--- script.js
+++ script.js
@@ -205,3 +205,32 @@ function nav() {
 }
 
 nav();
+
+
+new Swiper('.card-wrapper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    1024:{
+      slidesPerView: 3,
+    },
+  }
+});
